bc1f3fb1c8f0d617caba78a5832333964669c2c9
Michael Martinez <66656582+lonelydomino@users.noreply.github.com>
2022-06-06 13:54:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+####################################
+#Write your code below this line 👇
+
+sum = int(two_digit_number[0]) + int(two_digit_number[1])
+
+print(sum)
+print(float(sum))
+print(type(float(sum))) #Check type
+
+
+
+
+
